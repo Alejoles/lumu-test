f476c4d9e42126ed8bd09b2a7b20f1fd050efe69
Alejoles <alejo.uribe35@gmail.com>
2024-10-29 01:39:52 -0500

    Added english comments, updated README and docker-compose

--- README.md
+++ README.md
@@ -4,19 +4,79 @@
 ## Explain what is the computational complexity of your ranking algorithm.
 
 
+```python
+def print_client_ips_and_hosts_rank(data_ips, data_hosts):
+    """
+        This function is used to do the ranking of ips and hosts as
+        requested by the test, it will print the data as tables.
+    """
+    # Counting the occurrences of each IP
+    ip_counts = data_ips.value_counts()
+    total_ips = len(data_ips)
+
+    # Calculate the percentage of each IP
+    data_client_ips = [(ip, count, round((count / total_ips) * 100, 2)) for ip, count in ip_counts.items()]
+    print(f"Total records {total_ips}\n")
+    # Print first 5 results in table format
+    print(f"{'Client IPs Rank':<20} {'Count':>5} {'%':>6}")
+    print(f"{'-'*20} {'-'*5} {'-'*6}")
+    for ip, count, percent in data_client_ips[:5]:
+        print(f"{ip:<20} {count:>5} {percent:>6}%")
+    print(f"{'-'*20} {'-'*5} {'-'*6}")
+
+    print()
+
+    # Count the occurrences of each client_name
+    name_counts = data_hosts.value_counts()
+    total_names = len(data_hosts)
+
+    # Calculate the percentage of each client_name
+    data_host_rank = [(name, count, round((count / total_names) * 100, 2)) for name, count in name_counts.items()]
+
+
+    # Print first 5 results in table format
+    print(f"{'Host Rank':<50} {'Count':>5} {'%':>6}")
+    print(f"{'-'*50} {'-'*5} {'-'*6}")
+    for name, count, percent in data_host_rank[:5]:
+        print(f"{name:<50} {count:>5} {percent:>6}%")
+    print(f"{'-'*50} {'-'*5} {'-'*6}")
+```
+
 ## Provide instructions on how to run your script.
 
-Create an .env file an fulfill the fields using the .env.example file
+Create an .env file an fulfill the fields using the .env.example file as guide.
 
+### Running with venv
+(This commands are for ubuntu shell, may differ if you're using another OS)
+
+First you have to create the virtual evironment.
 ```sh
 python -m venv venv
+```
+
+Then you have to access to the virtual environment, type this command.
+
+```sh
 source venv/bin/activate
 ```
 
+Once inside type the command that is next to "$" symbol.
+
 ```sh
-pip install -r requirements.txt
+(venv) $ pip install -r requirements.txt
 ```
 
 ```sh
-python main.py
+(venv) $ python main.py
 ```
+
+### Running with docker
+(For all OS's)
+
+Just run
+
+```sh
+docker-compose up --build
+```
+
+Docker will create an image and a container based in the information of Dockerfile and docker-compose.yml, it will run the command and then after finishes the execution the container will stop.
--- main.py
+++ main.py
@@ -10,17 +10,17 @@ from settings import (
 
 def print_client_ips_and_hosts_rank(data_ips, data_hosts):
     """
-        Esta función sirve únicamente para mostrar los datos
-        ordenados de la forma en la que se pide en el ejercicio
+        This function is used to do the ranking of ips and hosts as
+        requested by the test, it will print the data as tables.
     """
-    # Contar las ocurrencias de cada IP
+    # Counting the occurrences of each IP
     ip_counts = data_ips.value_counts()
     total_ips = len(data_ips)
 
-    # Calcular el porcentaje de cada IP
+    # Calculate the percentage of each IP
     data_client_ips = [(ip, count, round((count / total_ips) * 100, 2)) for ip, count in ip_counts.items()]
     print(f"Total records {total_ips}\n")
-    # Imprimir primeros 5 resultados en formato de tabla
+    # Print first 5 results in table format
     print(f"{'Client IPs Rank':<20} {'Count':>5} {'%':>6}")
     print(f"{'-'*20} {'-'*5} {'-'*6}")
     for ip, count, percent in data_client_ips[:5]:
@@ -29,15 +29,15 @@ def print_client_ips_and_hosts_rank(data_ips, data_hosts):
 
     print()
 
-     # Contar las ocurrencias de cada client_name
+    # Count the occurrences of each client_name
     name_counts = data_hosts.value_counts()
     total_names = len(data_hosts)
 
-    # Calcular el porcentaje de cada client_name y tomar solo los primeros 5
+    # Calculate the percentage of each client_name
     data_host_rank = [(name, count, round((count / total_names) * 100, 2)) for name, count in name_counts.items()]
 
 
-    # Imprimir primeros 5 resultados en formato de tabla
+    # Print first 5 results in table format
     print(f"{'Host Rank':<50} {'Count':>5} {'%':>6}")
     print(f"{'-'*50} {'-'*5} {'-'*6}")
     for name, count, percent in data_host_rank[:5]:
@@ -45,9 +45,18 @@ def print_client_ips_and_hosts_rank(data_ips, data_hosts):
     print(f"{'-'*50} {'-'*5} {'-'*6}")
 
 
-def read_and_process_file(file_name):
+def read_and_process_file(file_name: str):
+    """
+        Main processing function, this function uses pandas to retrieve the
+        data from queries.txt file, then it
+        Args:
+            file_name (str): (the name of the file, in this case queries.txt)
+        returns:
+            records (list): a list of objects(dicts) that contains the data to do the requests
+                to the API.
+    """
     df = pd.read_csv(file_name, sep=" ", header=None)
-    # La API necesita los siguientes datos:
+    # the API needs the next data:
     #   timestamp(datetime), name(Array), client_ip(str), client_name(str), type(str)
     timestamps = pd.to_datetime(df[0] + ' ' + df[1])
     # print((timestamps))
@@ -77,14 +86,24 @@ def read_and_process_file(file_name):
 
 
 def make_requests(data: list):
+    """
+    Sends batches of DNS queries to a specified API endpoint.
+
+    Args:
+        data (list): A list of DNS query data to be sent in batches.
+
+    """
     headers = {'Content-Type': "application/json"}
     url_parsed = f"{API_URL}/collectors/{COLLECTOR_ID}/dns/queries?key={LUMU_CLIENT_KEY}"
-    sending_size = 300
+    sending_size = 500
     ammount_of_blocks = math.ceil(len(data) / sending_size)
     for i in range(ammount_of_blocks):
+        # Slice the data list to get the current batch
         data_to_json = data[i*sending_size:(i+1)*sending_size]
-        # print(len(data_to_json)) # Para ver que el último dato sea menor o igual a 500
+        # Send a POST request to the API with the current batch of data
         response = requests.post(url=url_parsed, headers=headers, json=(data_to_json))
+        # Optionally print the length of the current batch for debugging purposes
+        # print(len(data_to_json))  # To ensure the last batch is less than or equal to 50
 
 
 if __name__ == "__main__":
